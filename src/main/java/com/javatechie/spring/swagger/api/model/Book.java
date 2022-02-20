1a29d28d590ac134096358e7788eac08ddd78f57
yashwanth9724 <yashr7409@gmail.com>
2022-02-20 14:54:03 -0600

    Model Update

--- src/main/java/com/javatechie/spring/swagger/api/model/Book.java
+++ src/main/java/com/javatechie/spring/swagger/api/model/Book.java
@@ -33,4 +33,24 @@ public class Book implements Serializable {
 	private String bookName;
 	@ApiModelProperty(value = "this is book price")
 	private double price;
+	public int getBookId() {
+		return bookId;
+	}
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	
 }
